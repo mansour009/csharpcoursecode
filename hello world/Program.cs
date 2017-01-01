80764de05a136b5501c5ad89068efaf531a0ec84
mansour009 <mido.mansour@hotmail.com>
2017-01-01 22:44:20 +0300

    hello world
    this is my first try on c#

---
+++ hello world/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
