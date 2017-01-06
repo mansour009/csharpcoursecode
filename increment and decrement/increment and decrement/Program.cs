8df0bfe934292661ed7c036b28021e75907b5310
mansour009 <mido.mansour@hotmail.com>
2017-01-07 00:43:09 +0300

    increment and decremnt practice
    this is a simple practice to differentiate between i++ and ++i in c#

---
+++ increment and decrement/increment and decrement/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace increment_and_decrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1 = 5;
+            int temp = ++var1;
+            int temp1 = var1++;
+            double var2 = 7;
+            double var3 = 2.0;
+            double temp2 = var2-- / ++var3;
+
+            Console.WriteLine("prefix={0}\npostfix={1}",temp,temp1);
+            Console.WriteLine("equation ={0}", temp2);
+            //why ?
+            //because the decrement postfix assign the value of the temporary ,
+            //while the increment prefix assign it as new value after storing it as variable 
+
+            Console.ReadKey();
+        }
+    }
+}
