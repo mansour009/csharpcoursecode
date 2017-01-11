8e4c7b3eb4649d8f1ed9ab422ac4689dd5da7355
mansour009 <mido.mansour@hotmail.com>
2017-01-11 15:48:21 +0300

    while loop
    this is while loop challenge 12

---
+++ while loop/while loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myweight = 120 , x=0 ;
+          
+            while (myweight > 83) 
+            {
+                myweight--;
+                x++;
+            }
+            Console.WriteLine("this is how many exercise i should workout = {0}", x);
+                Console.ReadKey();
+            
+        }
+    }
+}
