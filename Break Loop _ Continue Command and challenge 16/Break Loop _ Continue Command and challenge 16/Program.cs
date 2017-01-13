5d9f8acac861ff7595a0790315e0273e544fb3ff
mansour009 <mido.mansour@hotmail.com>
2017-01-13 19:25:02 +0300

    Break Loop _ Continue Command and challenge 16
    this challenge shows the diffrent between "break" in the previous
    chaleng and "continue"

---
+++ Break Loop _ Continue Command and challenge 16/Break Loop _ Continue Command and challenge 16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Loop___Continue_Command_and_challenge_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multi = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0}", i);
+                if (i > 7)
+                  
+                continue;
+
+                for (int j = 1; j <= 5; j++)
+                {
+                    multi = i * j;
+                    Console.WriteLine("{0}*{1}={2}", i, j, multi);
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
