c70862e2abc91e943f8b909e8f72d4f291720546
mansour009 <mido.mansour@hotmail.com>
2017-01-13 18:50:55 +0300

    Break Loop_Break Command and challenge 15
    this callenge to test how" break" command work

---
+++ Break Loop_Break Command and challenge 15/Break Loop_Break Command and challenge 15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Loop_Break_Command_and_challenge_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1 :write multiply table from 1-10 and stop the loop when it is 7
+
+            int multi = 0;
+            for (int i = 1; i <= 10; i++) 
+            {
+                if (i > 7)
+                    break;
+                for (int j = 1; j <= 5; j++)
+                {
+                    multi = i * j;
+                    Console.WriteLine("{0}*{1}={2}", i, j, multi);
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Break Loop_Break Command and challenge 15/Break Loop_Break Command and challenge 15/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Break Loop_Break Command and challenge 15")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("Break Loop_Break Command and challenge 15")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1767ca19-7ef8-450a-8c27-1e0b04976c3e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
