b8d59f8ca8d64078272e9d04351b0075708dfdda
mansour009 <mido.mansour@hotmail.com>
2017-01-14 12:20:09 +0300

    convert command
    a simple practice on convert command and substring

---
+++ Convert Command and Challenge 18/Convert Command and Challenge 18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_Command_and_Challenge_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myfloat = 45.54f;
+            string mystring, substring;
+
+            mystring = Convert.ToString(myfloat);
+
+            substring = mystring.Substring(2,3);
+
+            Console.WriteLine($"my var = {mystring} \n my substring ={substring} ");
+            Console.ReadKey();
+
+        }
+    }
+}
