ef16a7b2246cb8d063cb07767aca2a02005a2815
mansour009 <mido.mansour@hotmail.com>
2017-01-14 22:40:17 +0300

    Enums Variables and Challenge 20
    in this lesson we wil deal with Enumerations variable and challenge is
    an exampe on that

---
+++ Enums Variables and Challenge 20/Enums Variables and Challenge 20/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_Variables_and_Challenge_20
+{
+    class Program
+    {
+        enum orientation : sbyte
+        { 
+            north =2,
+            east = 3,
+            south =4,
+            west = 5
+            }
+        static void Main(string[] args)
+        {
+            orientation mynorth = orientation.north, myeast = orientation.east, mysouth = orientation.south, mywest = orientation.west;
+            sbyte myval, myval1, myval2 ,myval3;
+
+            myval = (sbyte)(mynorth);
+            myval1 = (sbyte)(mysouth);
+            myval2 = (sbyte)(myeast);
+            myval3 = (sbyte)(mywest);
+
+            Console.WriteLine($"my directions is:\n{mynorth}and equal= {myval}\n{myeast}and equal= {myval2}\n{mysouth}and equal= {myval1}\n{mywest}and equal= {myval3}");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
