6d39545ad2661846be8c84dd66075ac8358d1290
mansour009 <mido.mansour@hotmail.com>
2017-01-16 12:10:41 +0300

    array forloop
    how to use array and find max value in your assigned array

---
+++ array for loop and challenge 22/array for loop and challenge 22/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_for_loop_and_challenge_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int myarray = 6;
+            int[] mynumbers = new int [myarray] { 1, 5, 9, 11, 12,15 };
+            int[] mynumbers1 = new int[myarray] { 5, 10, 15, 20, 25, 30 };
+            int max ;
+            for (int i = 0; i < 6; i++)
+            {
+               
+                Console.WriteLine("my numbers " + (i + 1) + " = {0}", mynumbers1[i]);
+            }
+                max = mynumbers1.Max();
+                Console.WriteLine("\nmy maximum value = {0}",max);
+            
+            Console.ReadKey();
+            
+        }
+    }
+}
