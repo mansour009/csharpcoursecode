96746b470408c5030723dba850c3bba8cb4aa2a3
mansour009 <mido.mansour@hotmail.com>
2017-01-18 23:25:20 +0300

    string manipulation and challenge 25
    this challenge on string manipulation and challenge 25

---
+++ string operation/string operation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_operation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myvar = "I AM smart";
+            myvar = myvar.Substring(4);
+            myvar = "he is" + myvar;
+
+            char[] myvar1 = myvar.ToCharArray();
+           
+
+            Console.WriteLine("{0}",myvar1.Length);
+            for(int i=0;i<myvar1.Length;i++)
+            {
+               
+
+
+                Console.Write($"{myvar1[i]}");
+                
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
