edbd09da46c0879f269a9c1f955c51ccc40f5ebc
mansour009 <mido.mansour@hotmail.com>
2017-01-20 15:32:23 +0300

    out parameter and challenge 28
    what is the difference between out parameter and refrence parameter

---
+++ out prameter challeng 28/out prameter challeng 28/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace out_prameter_challeng_28
+{
+    class Program
+    {
+        //Q.28 write an out prameter programe
+        static int mygrades(int[]myarray,out int myindex)
+        {
+            int maxvalue = myarray[0];
+            myindex = 0;
+            for(int i=1;i<myarray.Length;i++)
+            {
+                if (myarray[i] > maxvalue)
+                {
+                    
+                    maxvalue = myarray[i];
+                    myindex = i;
+                }
+
+            }
+            return maxvalue;
+        }
+        static void Main(string[] args)
+{
+            int myindex;
+            int[] myarray = { 12, 52, 36, 40, 60, 26, 36, 89, 54};
+          int maxvalue=  mygrades(myarray, out myindex);
+            Console.Write($"{maxvalue},{myindex}");
+            Console.ReadKey();
+
+        }
+    }
+}
