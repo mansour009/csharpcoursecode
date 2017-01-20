9394f7f6706b04fb61b90f5d9500243e4fbad0e9
mansour009 <mido.mansour@hotmail.com>
2017-01-20 14:37:32 +0300

    reference values and challeng 27
    another practice on functions and new challeng

---
+++ reference values and challenge 27/reference values and challenge 27/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reference_values_and_challenge_27
+{
+    class Program
+    {
+        static void myltiply (ref int myvar1 ,ref int myvar2)
+        {
+            myvar1 *= 4; myvar2 *= 4; 
+
+            Console.WriteLine("{0},{1}",myvar1,myvar2);
+        }
+      
+        static void Main(string[] args)
+        {
+            int number = 6, number1 = 10;
+
+            Console.WriteLine("my first int{0},my secound int{1}",number,number1);
+            myltiply(ref number,ref number1);
+            Console.WriteLine("my first int{0},my secound int{1}", number, number1);
+
+
+            Console.ReadKey();
+        }
+    }
+}
