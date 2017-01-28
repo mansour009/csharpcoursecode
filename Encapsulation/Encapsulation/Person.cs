99da1bf8f5d391e297748c698a18d9d754c93770
mansour009 <mido.mansour@hotmail.com>
2017-01-28 14:46:07 +0300

    Encapsulation
    Encapsulation basic practice

---
+++ Encapsulation/Encapsulation/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        public void PrintValues()
+        { Console.WriteLine($"my name is {name} and my age is {age}"); }
+
+        public Person() { }
+        public Person(string name, int age)
+{
+        this.name =name ;
+          this.age=age ;
+            }
+        
+        public void SetName (string name)
+        {
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetAge (int age)
+        {
+            this.age = age;
+
+        }
+        public int GetAge ()
+        {
+            return age;
+
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = " AHMED MANSOUR";
+            int age = 24;
+
+            Person myobj = new Person() ;
+            myobj.SetName (name);
+            myobj.SetAge(age);
+
+            Console.WriteLine("my name is" + myobj.GetName());
+            Console.WriteLine("my age is" + myobj.GetAge());
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
